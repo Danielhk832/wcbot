aec746cc7b74f38509117dbb5caad9b659b01ce1
Daniel Kish <aox.soljah@gmail.com>
2022-01-16 07:02:00 +0700

    added words and inital migration setup completed

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,11 @@
 exports.up = async (knex) => {
-  await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
-    })
-}
+  await knex.schema.createTable("words", (words) => {
+    words.increments("word_id");
+    words.string("word").notNullable();
+    words.integer("value").notNullable();
+  });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema.dropTableIfExists("words");
+};
